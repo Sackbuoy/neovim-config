f33a28bb7bee4c95d541432af0f60f71421fe9a7
Cameron Kientz <47261410+Sackbuoy@users.noreply.github.com>
2023-11-09 17:48:01 -0600

    consolidating and adding clipboard shortcuts

--- init.lua
+++ init.lua
@@ -1,36 +1,43 @@
 require('plugins')
 
--- line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
+local opt = vim.opt
+local g = vim.g
+local cmd = vim.cmd
+local keymap = vim.keymap
+local api = vim.api
+local fn = vim.fn
 
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
+-- line numbers
+opt.number = true
+opt.relativenumber = true
+
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.expandtab = true
 
 -- other stuff idk how to categorize
-vim.opt.showmatch = true
-vim.opt.incsearch = false
+opt.showmatch = true
+opt.incsearch = false
 
 -- scrolling
-vim.opt.scrolloff = 12
-vim.opt.ttyfast  = true-- speed up scrolling
-vim.opt.mouse = '' -- dumb that i have to set it to empty string
+opt.scrolloff = 12
+opt.ttyfast  = true-- speed up scrolling
+opt.mouse = '' -- dumb that i have to set it to empty string
 
 -- visuals/colors
-vim.opt.signcolumn = 'yes'
-vim.opt.colorcolumn = '80'
-vim.opt.termguicolors = true -- some themes need this
+opt.signcolumn = 'yes'
+opt.colorcolumn = '80'
+opt.termguicolors = true -- some themes need this
 
 -- idk honestly
-vim.opt.hidden = true -- keep buffers open
-vim.opt.completeopt=menu,menuone,noselect
+opt.hidden = true -- keep buffers open
+opt.completeopt=menu,menuone,noselect
 
 -- set leader
-vim.g.mapleader = ' '
+g.mapleader = ' '
 
-vim.cmd.colorscheme('OceanicNext')
+cmd.colorscheme('OceanicNext')
 
 -- Telescope
 
@@ -45,44 +52,48 @@ _G.project_files = function()
     end 
 end 
 
-vim.keymap.set('n', '<leader>ff', '<cmd>lua project_files()<CR>', {noremap=true})
-vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>')
-vim.keymap.set('n', '<space>a', '<cmd>Telescope aerial<CR>', {noremap=true, silent=true})
+keymap.set('n', '<leader>ff', '<cmd>lua project_files()<CR>', {noremap=true})
+keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>')
+keymap.set('n', '<space>a', '<cmd>Telescope aerial<CR>', {noremap=true, silent=true})
 
 -- NERDTree keybinds
-vim.keymap.set('n', '<leader>l', ':NERDTreeToggle<CR>')
-vim.keymap.set('n', '<leader>d', ':NERDTreeFind<CR>')
+keymap.set('n', '<leader>l', ':NERDTreeToggle<CR>')
+keymap.set('n', '<leader>d', ':NERDTreeFind<CR>')
 
 -- Global Keybinds
-vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>')
-vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
-vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
-vim.keymap.set('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>')
-vim.keymap.set('n', '<leader>u', ':UndotreeToggle<CR>')
-vim.keymap.set('n', '<leader>E', ':Telescope diagnostics<CR>')
+keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>')
+keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+keymap.set('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>')
+keymap.set('n', '<leader>u', ':UndotreeToggle<CR>')
+keymap.set('n', '<leader>E', ':Telescope diagnostics<CR>')
 
 -- LSP keybinds
-vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>')
-vim.keymap.set('n', 'gi', ':Telescope lsp_implementations<CR>')
-vim.keymap.set('n', 'gd', ':Telescope lsp_definitions<CR>')
+keymap.set('n', 'gr', ':Telescope lsp_references<CR>')
+keymap.set('n', 'gi', ':Telescope lsp_implementations<CR>')
+keymap.set('n', 'gd', ':Telescope lsp_definitions<CR>')
+
+-- system clipboard shortcuts
+keymap.set('v', '<leader>y', '"+y')
+keymap.set('v', '<leader>p', '"+p')
 
 -- rust.vim
-vim.g.rust_clip_command = 'pbcopy' -- for mac
--- vim.g.rust_clip_command = 'xclip -selection clipboard' --for linux
+g.rust_clip_command = 'pbcopy' -- for mac
+-- g.rust_clip_command = 'xclip -selection clipboard' --for linux
 
--- vim-go
-vim.g.go_gopls_enabled = 0 -- im doing gopls myself so i get universal keybinds
+-- go
+g.go_gopls_enabled = 0 -- im doing gopls myself so i get universal keybinds
 
 -- function for executing a command that rewrites the buffer,
 -- and then restores the cursor to its original location before the exec
 _G.retain_window_exec = function(args)
-  local view = vim.fn.winsaveview()
-  vim.cmd.execute(arg)
-  vim.fn.winrestview(view)
+  local view = fn.winsaveview()
+  cmd.execute(arg)
+  fn.winrestview(view)
 end
 
 -- autocmd for running my formatter on go files
-vim.api.nvim_create_autocmd('BufWritePre', {
+api.nvim_create_autocmd('BufWritePre', {
   pattern = '*.go',
   command = "lua retain_window_exec('silent %!myfmt')",
 })
