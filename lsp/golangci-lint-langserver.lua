ed59f21b26ae42334528bbedf1e0316248338d35
Sackbuoy <cameronkientz@proton.me>
2026-04-15 14:17:14 -0500

    gci autorun - disabled for now

--- lsp/golangci-lint-langserver.lua
+++ lsp/golangci-lint-langserver.lua
@@ -10,19 +10,28 @@ return {
  init_options = {
    command = { "golangci-lint", "run", "--output.json.path", "stdout", "--show-stats=false" },
  },
- on_attach = function(client, bufnr)
-   vim.api.nvim_create_autocmd("BufWritePre", {
-     buffer = bufnr,
-     callback = function()
-       local filename = vim.fn.expand("%")
-       vim.fn.jobstart({ "golangci-lint", "run", "--fix", filename }, {
-         on_exit = function()
-           vim.schedule(function()
-             vim.cmd("silent! checktime")
-           end)
-         end,
-       })
-     end,
-   })
- end,
+  on_attach = function(client, bufnr)
+    -- vim.api.nvim_create_autocmd("BufWritePost", {
+    --   buffer = bufnr,
+    --   callback = function()
+    --     local filename = vim.fn.expand("%:p")
+    --     vim.fn.system({ "gci", "write", "--skip-generated", filename })
+    --     vim.cmd("silent! checktime")
+    --   end,
+    -- })
+    -- Run golangci-lint --fix asynchronously after the file is written
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      buffer = bufnr,
+      callback = function()
+        local filename = vim.fn.expand("%:p")
+        vim.fn.jobstart({ "golangci-lint", "run", "--fix", filename }, {
+          on_exit = function()
+            vim.schedule(function()
+              vim.cmd("silent! checktime")
+            end)
+          end,
+        })
+      end,
+    })
+  end,
 }
